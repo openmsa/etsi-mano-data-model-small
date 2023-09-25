bdcbda2cf48b612ed5048472849edc7ae48d30d5
Olivier Vignaud <ovi@ubiqube.com>
2023-09-25 16:12:45 +0200

    Remove estra licenses.

--- mano-db-api-version/src/main/java/com/ubiqube/etsi/mano/dao/mano/version/ApiVersionInformation.java
+++ mano-db-api-version/src/main/java/com/ubiqube/etsi/mano/dao/mano/version/ApiVersionInformation.java
@@ -14,20 +14,6 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see https://www.gnu.org/licenses/.
  */
-/**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
- *
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/
- *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
- */
 package com.ubiqube.etsi.mano.dao.mano.version;
 
 import java.io.Serializable;
@@ -37,7 +23,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
