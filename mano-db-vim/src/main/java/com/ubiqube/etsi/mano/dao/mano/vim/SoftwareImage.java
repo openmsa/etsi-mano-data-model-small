007ecf08895e49d051e55714558d8365a6b4b7dc
Olivier Vignaud <ovi@ubiqube.com>
2024-06-01 13:29:12 -0400

    🐛 vim: Add notNull annotation in SoftwareImage for name field.

--- mano-db-vim/src/main/java/com/ubiqube/etsi/mano/dao/mano/vim/SoftwareImage.java
+++ mano-db-vim/src/main/java/com/ubiqube/etsi/mano/dao/mano/vim/SoftwareImage.java
@@ -34,6 +34,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -56,6 +57,7 @@ public class SoftwareImage implements Auditable {
 
 	private String vimId;
 
+	@NotNull
 	private String name;
 
 	private String provider;
