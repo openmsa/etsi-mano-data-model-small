4b611f6a1b1e63d7ddacc8ce0a1292e85925709e
Olivier Vignaud <ovi@ubiqube.com>
2024-06-02 21:18:27 +0200

    ✨ Add TLS information to KubernetesV1Auth.

--- mano-db-connection/src/main/java/com/ubiqube/etsi/mano/dao/mano/ai/KubernetesV1Auth.java
+++ mano-db-connection/src/main/java/com/ubiqube/etsi/mano/dao/mano/ai/KubernetesV1Auth.java
@@ -16,15 +16,33 @@
  */
 package com.ubiqube.etsi.mano.dao.mano.ai;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ubiqube.etsi.mano.dao.mano.AccessInfo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @Entity
+@SuperBuilder
 public class KubernetesV1Auth extends AccessInfo {
 
 	private static final long serialVersionUID = 1L;
+
+	@Column(length = 5000)
+	@JsonProperty("certificate-authority-data")
+	private String certificateAuthorityData;
+
+	private String url;
+
+	@Column(length = 5000)
+	@JsonProperty("client-certificate-data")
+	private String clientCertificateData;
+
+	@Column(length = 5000)
+	@JsonProperty("client-key-data")
+	private String clientKeyData;
 
 }
