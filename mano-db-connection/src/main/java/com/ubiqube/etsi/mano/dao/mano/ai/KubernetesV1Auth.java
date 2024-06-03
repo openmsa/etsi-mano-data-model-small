d51defdd9c1de94f795b79eabea698208ca9eae3
Olivier Vignaud <ovi@ubiqube.com>
2024-06-03 14:38:14 +0200

    🚑️ connection: Add @NoArgsConstructor on KubernetesV1Auth.

--- mano-db-connection/src/main/java/com/ubiqube/etsi/mano/dao/mano/ai/KubernetesV1Auth.java
+++ mano-db-connection/src/main/java/com/ubiqube/etsi/mano/dao/mano/ai/KubernetesV1Auth.java
@@ -22,10 +22,12 @@ import com.ubiqube.etsi.mano.dao.mano.AccessInfo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @Entity
+@NoArgsConstructor
 @SuperBuilder
 public class KubernetesV1Auth extends AccessInfo {
 
