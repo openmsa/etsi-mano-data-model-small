4203a11aca2ab078cc87703a8813ebaa096f61a6
Olivier Vignaud <ovi@ubiqube.com>
2024-07-09 11:50:59 -0400

    🎨 vim: Change `ClusterOptionVersion` to plain entity.

--- mano-db-vim/src/main/java/com/ubiqube/etsi/mano/dao/mano/vim/vnfi/ClusterOptionVersion.java
+++ mano-db-vim/src/main/java/com/ubiqube/etsi/mano/dao/mano/vim/vnfi/ClusterOptionVersion.java
@@ -1,15 +1,27 @@
 package com.ubiqube.etsi.mano.dao.mano.vim.vnfi;
 
 import java.io.Serializable;
+import java.util.UUID;
 
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-@Embeddable
+@Entity
 public class ClusterOptionVersion implements Serializable {
 	/** Serial. */
 	private static final long serialVersionUID = 1L;
+	/**
+	 * Vim ID, same as vimId in {@link} VimConnectionInformation}.
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@NotNull
+	private UUID id;
 
 	private String module;
 
