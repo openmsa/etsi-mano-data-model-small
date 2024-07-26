8cf13bd3ef1ed2c5534caf5ec31690a68e4f95f8
Olivier Vignaud <ovi@ubiqube.com>
2024-07-26 12:03:04 -0400

    🐛 connection: Change connection table to `@MappedSuperclass`.

--- mano-db-connection/src/main/java/com/ubiqube/etsi/mano/dao/mano/Connection.java
+++ mano-db-connection/src/main/java/com/ubiqube/etsi/mano/dao/mano/Connection.java
@@ -23,21 +23,18 @@ import java.util.UUID;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@MappedSuperclass
 public class Connection<I extends InterfaceInfo, A extends AccessInfo> implements Serializable {
 	/** Serial. */
 	private static final long serialVersionUID = 1L;
