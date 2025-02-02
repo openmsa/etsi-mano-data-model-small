786f539560655ab2a8925a3aadd60e7cbba261e9
Olivier Vignaud <ovi@ubiqube.com>
2025-02-02 18:25:36 +0100

    🧪 connection: Add unit test.

--- mano-db-connection/src/main/java/module-info.java
+++ mano-db-connection/src/main/java/module-info.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (C) 2019-2025 Ubiqube.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
  */
 module com.ubiqube.etsi.mano.dao.mano {
 	exports com.ubiqube.etsi.mano.dao.mano;
@@ -25,4 +25,5 @@ module com.ubiqube.etsi.mano.dao.mano {
 	requires jakarta.persistence;
 	requires jakarta.validation;
 	requires com.fasterxml.jackson.annotation;
+	requires java.desktop;
 }
