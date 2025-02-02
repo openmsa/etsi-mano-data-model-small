33d6c704178cf48934650d8612cf8ae3f933c463
Olivier Vignaud <ovi@ubiqube.com>
2025-02-02 11:59:32 +0100

    🧪 vim: Add unit tests.

---
+++ mano-db-vim/src/test/java/com/ubiqube/etsi/mano/dao/mano/vim/SpringH2Test.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2019-2025 Ubiqube.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.dao.mano.vim;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EntityScan({ "com.ubiqube.etsi.mano.vim" })
+class SpringH2Test {
+
+	@Autowired
+	DataSource ds;
+
+	@Test
+	void testH2Schema() throws Exception {
+		assertTrue(true);
+	}
+
+}
