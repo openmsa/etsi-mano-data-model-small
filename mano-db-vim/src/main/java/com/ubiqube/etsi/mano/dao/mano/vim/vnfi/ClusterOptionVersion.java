81b9289b7ee46572f8afff542f6e272b38d2e5ac
Olivier Vignaud <ovi@ubiqube.com>
2025-02-13 15:06:57 +0100

    🎨 vim: Add Constructor on ClusterOpetionVersion.

--- mano-db-vim/src/main/java/com/ubiqube/etsi/mano/dao/mano/vim/vnfi/ClusterOptionVersion.java
+++ mano-db-vim/src/main/java/com/ubiqube/etsi/mano/dao/mano/vim/vnfi/ClusterOptionVersion.java
@@ -26,8 +26,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 public class ClusterOptionVersion implements Serializable {
